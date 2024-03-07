add4f6f745c1e01cdb39cd858b2f48b11ff689b8
Michael Hutchings <mhutchings0416@gmail.com>
2024-03-07 13:37:17 -0700

    Final push for submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
+// This is the app.tsx file
+
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import CollegeBasketballTeams from './CollegeBasketballTeams.json';
+
+interface CollegeTeams {
+  school: string;
+  name: string;
+  pop: number;
+  city: string;
+  state: string;
+}
+
+// This is a FUNCTION
+function Welcome() {
+  return <h1>College Basketball Teams</h1>;
+}
+
+// This is a COMPONENT
+class Team extends React.Component<CollegeTeams> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div
+        style={{
+          border: '3px solid black',
+          borderRadius: '10px',
+          width: '300px',
+          textAlign: 'center',
+          margin: '0 auto',
+          marginBottom: '10px',
+        }}
+      >
+        <br />
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot Name: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+        <br />
+      </div>
+    );
+  }
+}
+
+// ... Is the spread which takes all of the elements of the band names and spreading them out into an array or class to access the individual ElementFlags.
+function TeamList() {
+  return (
+    <div>
+      {CollegeBasketballTeams.teams.map((oneTeam) => (
+        <Team {...oneTeam} />
+      ))}
+    </div>
+  );
+}
+
+// This is the app function that we are calling in the index
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p> Hello, Michael was here. </p>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+// Michael Hutchings
+// Section 2
+// This is the index.tsx file that is first called.
+// We call the App.tsx file here and render it.
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -5,12 +10,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
